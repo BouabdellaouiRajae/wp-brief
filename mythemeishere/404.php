365f26dd654c6b8211df8a22788ba0859586dae4
BouabdellaouiRajae <77007702+BouabdellaouiRajae@users.noreply.github.com>
2021-04-12 14:36:25 +0200

    delete old themes

--- mythemeishere/404.php
+++
@@ -1,6 +0,0 @@
-<div class="container text-conter page-not-found">
-
-<a href="index.php"><img src="<?php echo get_template_directory_uri() .?>" alt="Page Not found image"/> </a>
-
-</div>
-<?php get_footer();?>
